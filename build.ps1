5e26d92d8436d49cb321c7c0418cf3f53655759a
Stefano Sinigardi <stesinigardi@hotmail.com>
2025-08-30 01:35:34 +0200

    remove old ci - part 1 of N

--- build.ps1
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pwsh
-
-Remove-Item .\build -Force -Recurse
-New-Item .\build -itemtype directory -Force
-Set-Location build
-cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" ..
-cmake --build . --target install
-Set-Location ..
